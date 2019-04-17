830105226bf6fb755fb59deb3cfd44d83b0593d3
tigeryzx <nekomail@qq.com>
2019-04-17 17:53:14 +0800

    初始化配置生成项目

---
+++ tools/GConfig/GConfig/Program.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GConfig
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('s', "setting", Required = true, HelpText = "Setting file.")]
+            public string Setting { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (!string.IsNullOrEmpty(o.Setting))
+                       {
+                           var settingPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, o.Setting);
+                           Console.WriteLine($"input path {settingPath}");
+                       }
+                   });
+
+            
+        }
+    }
+}
